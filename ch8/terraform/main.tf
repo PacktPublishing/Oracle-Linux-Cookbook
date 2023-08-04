0e5a0dd1b5ba5744e4bd70a989d4e2ee3aad5f8f
Erik Thomsen <etho201@gmail.com>
2023-08-04 01:00:39 -0400

    Updated main.tf

--- ch8/terraform/main.tf
+++ ch8/terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     oci = {
-      source  = "hashicorp/oci"
-      version = "4.57.0"
+      source  = "oracle/oci"
+      version = "5.7.0"
     }
   }
 }
@@ -19,17 +19,16 @@ provider "oci" {
 # Generate SSH Key Pair for access
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
-  rsa_bits  = 4096
 }
-resource "local_file" "ssh_private_key" {
+resource "local_sensitive_file" "ssh_private_key" {
   filename          = "${path.module}/instances/${var.instance_name}/id_rsa"
   file_permission   = "600"
-  sensitive_content = tls_private_key.ssh.private_key_pem
+  content = tls_private_key.ssh.private_key_pem
 }
 
 # Get a list of Availability Domains
 data "oci_identity_availability_domains" "ads" {
-  compartment_id = var.tenancy_ocid
+  compartment_id = var.compartment_ocid
 }
 
 # Create an OCI core instance
@@ -39,7 +38,7 @@ resource "oci_core_instance" "ol_instance" {
   ]
   display_name        = var.instance_name
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
-  compartment_id      = var.tenancy_ocid
+  compartment_id      = var.compartment_ocid
   shape               = "VM.Standard.E2.1.Micro"
 
   source_details {
@@ -49,11 +48,9 @@ resource "oci_core_instance" "ol_instance" {
   create_vnic_details {
     assign_public_ip = true
     subnet_id        = var.subnet_ocid
-    nsg_ids          = [var.nsg_ocid]
   }
   metadata = {
     ssh_authorized_keys = tls_private_key.ssh.public_key_openssh
-    user_data           = "${base64encode(file("./scripts/provision.sh"))}"
   }
 }
 
@@ -65,23 +62,4 @@ output "connection_details" {
 resource "local_file" "readme" {
   filename = "${path.module}/instances/${var.instance_name}/README.md"
   content  = oci_core_instance.ol_instance.public_ip
-}
-resource "oci_core_network_security_group_security_rule" "http" {
-  #Required
-  network_security_group_id = var.nsg_ocid
-  direction                 = "INGRESS"
-  protocol                  = "6"
-
-  description = "http"
-  source      = "0.0.0.0/0"
-  source_type = "CIDR_BLOCK"
-  tcp_options {
-
-    #Optional
-    destination_port_range {
-      #Required
-      max = 80
-      min = 80
-    }
-  }
 }
